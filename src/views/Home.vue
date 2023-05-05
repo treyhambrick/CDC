c5e18f126a2351e8976b63f178dd78bd3e2ed769
treyhambrick <trey.hambrick@gmail.com>
2023-05-05 09:15:44 -0400

    first commit123

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,34 @@
-<template>
+<template> 
   <Gallery />
+  
+  <div v-if="!isMobile()">
+     not mobile device
+  </div>
+  <div v-else>
+     yes mobile device
+  </div>
 </template>
 
 
 <script>
-// notes: https://travishorn.com/creating-a-photo-gallery-with-vue-css-grid-3e0a3dd25285
+// notes: https://travishorn.com/creating-a-photo-gallery-with-vue-css-grid-3e0a3dd25285  isMobile = {{ isMobile() }}
 // cd C:\Users\thambrick\photo-gallery
 // npm run serve
 import Gallery from '@/components/Gallery.vue';
-
 
 export default {
   name: 'home',
   components: {
     Gallery,
+  },
+  methods: {
+    isMobile() {
+      if (screen.width <= 760) {
+        return true
+      } else {
+        return false
+      }
+    },
   },
 };
 </script>
