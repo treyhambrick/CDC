524ad67ef3c12fb31cf7b0a033b3b7b0c2357042
treyhambrick <trey.hambrick@gmail.com>
2023-05-06 08:35:04 -0400

    first commit123

--- run.bat
+++ run.bat
@@ -1,15 +1,8 @@
 git init
-PAUSE
 git add .
-PAUSE
 git commit -m "first commit123"
-PAUSE
 git branch -M main
-PAUSE
 git remote add origin https://github.com/treyhambrick/cdc.git
-PAUSE
 git push -u origin main
-PAUSE
 git add dist && git commit -m "Initial dist subtree commit"
-PAUSE
 git subtree push --prefix dist origin gh-pages
