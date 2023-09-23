a07f0e2ddab29040e463f766f901bae2e8325912
treyhambrick <trey.hambrick@gmail.com>
2023-09-23 15:16:32 -0400

    first commit1234

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 git init
 git add .
-git commit -m "first commit123"
+git commit -m "first commit1234"
 git branch -M main
 git remote add origin https://github.com/treyhambrick/cdc.git
 git push -u origin main
